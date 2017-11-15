38328ed80deab0d9c756c4541c93e6f8af946bb0
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2017-11-15 22:37:34 +0000

    use server timer

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -1,5 +1,6 @@
 -module(ws_handler).
 -define(TIMEOUT, 3600000).
+-define(DELAY, 500).
 
 -export([init/2]).
 
@@ -12,42 +13,32 @@ init(Req, _State) ->
     {cowboy_websocket, Req, initial_state, #{idle_timeout => ?TIMEOUT}}.
 
 handle_message([{<<"start">>, [{<<"width">>, Width}, {<<"height">>, Height}]}], initial_state) ->
-    Name = list_to_atom("grid-" ++ integer_to_list(erlang:unique_integer())),
-    {ok, _Pid} = conway_sup:start_child(Name, Width, Height),
-    ok = conway_gen_server:start(Name, Width, Height),
-    {ok, Name};
-handle_message([{<<"start">>, [{<<"width">>, Width}, {<<"height">>, Height}]}], Name) ->
-    ok = conway_gen_server:start(Name, Width, Height),
-    {ok, Name};
-handle_message([{<<"next">>, _}], initial_state = S) ->
-    erlang:send_after(250, self(), next),
+    {ok, Pid} = conway_sup:start_child(Width, Height),
+    ok = conway_gen_server:start(Pid, Width, Height),
+    erlang:send_after(?DELAY, self(), next),
+    {ok, {started, Pid}};
+handle_message([{<<"start">>, [{<<"width">>, Width}, {<<"height">>, Height}]}], {started, Pid} = S) ->
+    ok = conway_gen_server:start(Pid, Width, Height),
     {ok, S};
-handle_message([{<<"next">>, _}], Name) ->
-    %% try conway_gen_server:next(Name) of
-    %% 	Reply -> {reply, {text, Reply}, Req, Name}
-    %% catch
-    %% 	A:B ->
-    %% 	    lager:error("ERROR ~p~p~n",[A,B]),
-    %% 	    {ok, Req, Name}
-    %% end.
-    Grid = conway_gen_server:next(Name),
-    ListGrid = sets:fold(fun({X,Y}, Acc) -> [[X,Y] | Acc] end, [], Grid),
-    {reply, {text, jsx:encode([{alive, ListGrid}])}, Name}.
+handle_message(_, S) ->
+    {ok, S}.
 
 websocket_handle({text, Msg}, State) ->
     handle_message(jsx:decode(Msg), State);
 websocket_handle(_Frame, State) ->
     {ok, State}.
 
-websocket_info(next, initial_state = S) ->
-    lager:error("Out of order init",[]),
-    {ok, S};
-websocket_info(next, Name) ->
-    handle_message([{<<"next">>, 1}], Name);
-websocket_info(_Info, State) ->
+websocket_info(next, {started, Pid}=S) ->
+    Grid = conway_gen_server:next(Pid),
+    ListGrid = sets:fold(fun({X,Y}, Acc) -> [[X,Y] | Acc] end, [], Grid),
+    erlang:send_after(?DELAY, self(), next),
+    {reply, {text, jsx:encode([{alive, ListGrid}])}, S};
+websocket_info(_, State) ->
     {ok, State}.
 
-terminate(Reason, _, Name) ->
-    conway_gen_server:stop(Name),
+terminate(Reason, _, {started, Pid}) ->
+    conway_gen_server:stop(Pid),
     lager:info("WS TERMINATE ~p~n",[Reason]),
+    ok;
+terminate(_, _, _) ->
     ok.
